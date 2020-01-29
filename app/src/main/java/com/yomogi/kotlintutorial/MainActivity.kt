d551b34cc1e4ba6cce88eea67c3f18765bc7aee4
DaiShiba <Yomogitablet@gmail.com>
2020-01-29 23:45:48 +0900

    びるどたいぷのチェック

--- app/src/main/java/com/yomogi/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/yomogi/kotlintutorial/MainActivity.kt
@@ -2,13 +2,13 @@ package com.yomogi.kotlintutorial
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text_version.text = BuildConfig.APPLICATION_ID
     }
 }
